222a044e10181ef466d19d77f314460a6a58abc5
tomasz_ciesielczyk <tomaszc@man.poznan.pl>
2026-03-18 13:19:29 +0100

    dt models

--- tm-service/tm/models/digital_twin.py
+++ tm-service/tm/models/digital_twin.py
@@ -1,3 +1,4 @@
+from dataclasses import dataclass
 from typing import Optional
 
 from pydantic import BaseModel
@@ -10,3 +11,39 @@ class DigitalTwinDAO(BaseModel):
     dt_uri: str
     update_ts: Optional[int] = None
     ext: Optional[str] = None
+
+
+@dataclass
+class DTModelDAO(BaseModel):
+    isp_len:  int
+    isp_unit:  int
+    model_id: Optional[int] = None
+    model_uri: Optional[int] = None
+    model_name: Optional[int] = None
+    model_description: Optional[int] = None
+    update_ts: Optional[int] = None
+    ext: Optional[str] = None
+
+
+
+@dataclass
+class DTForecastInfoDAO:
+    ts: int
+    isp_len: int
+    isp_unit: int
+    update_ts: Optional[int] = None
+    dt_id: Optional[int] = None
+    forecast_uri: Optional[str] = None
+    sequence: Optional[str] = None
+    offer_id: Optional[int] = None
+    model_id: Optional[int] = None
+
+
+@dataclass
+class DTForecastDAO:
+    forecast_id: int
+    isp_start: int
+    range_id: int
+    cost_mwh: Optional[float]
+    ts: int
+    isp_len: int = 1
